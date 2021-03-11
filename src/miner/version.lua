b13c3ee2c574f8cd986040fd392b2d7a67b5ef0d
zgyr <0112d759@gmail.com>
2021-03-11 17:33:02 +0600

    new file: miner/version.lua

---
+++ src/miner/version.lua
@@ -0,0 +1,17 @@
+return {
+  '0.0.0.1',
+  files = {
+    'bootloader.lua',
+    'version.lua',
+    'init.lua',
+    'globals.lua',
+    'move.lua',
+    'energy.lua',
+    'inventory.lua',
+    'scan.lua',
+    'tool.lua',
+    'compass.lua',
+    'assembler.lua',
+    'network.lua',
+  }
+}
